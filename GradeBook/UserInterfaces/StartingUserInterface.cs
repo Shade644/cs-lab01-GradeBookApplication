89c788364633de8cd2cecd60be983ce4b2717aed
Adrian Włodarczyk <33695278+Shade644@users.noreply.github.com>
2022-04-03 15:59:39 +0200

    18/24

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -36,14 +36,32 @@ namespace GradeBook.UserInterfaces
             var parts = command.Split(' ');
             if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                  Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            //BaseGradeBook gradeBook = new BaseGradeBook(name);
+            var type = parts[2];
+
+            BaseGradeBook gradeBook;
+
+            if (type == "standard")
+            {
+                gradeBook = new StandardGradeBook(name);
+            }
+            else if (type == "ranked")
+            {
+                gradeBook = new RankedGradeBook(name);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                return; ;
+            }
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
+
 
         public static void LoadCommand(string command)
         {
